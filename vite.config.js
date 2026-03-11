5ff7bac5b6a600df51101801512e4eea601788e3
Prince <kingofwealth2@gmail.com>
2026-03-11 13:40:41 +0000

    fix: split recharts into separate chunk to resolve d3 TDZ error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['recharts'],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom'],
+          'vendor-recharts': ['recharts'],
+        },
+      },
+    },
+  },
 })
